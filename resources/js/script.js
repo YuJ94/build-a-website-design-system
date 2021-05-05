bd693d4d314ea0cad1ecbb84dbe30c828798bff9
unknown <jyu8369@yahoo.com>
2021-05-04 21:20:05 -0500

    Updated sections and added javascript.

---
+++ resources/js/script.js
@@ -0,0 +1,24 @@
+const menuBtn = document.querySelector(".menu-button");
+const closeBtn = document.querySelector(".close-button");
+const navigation = document.querySelector(".navigation");
+const navigationItems = document.querySelectorAll(".navigation ul li a");
+
+menuBtn.addEventListener("click", () => {
+    menuBtn.classList.toggle("active");
+    closeBtn.classList.toggle("active");
+    navigation.classList.toggle("active");
+});
+
+closeBtn.addEventListener("click", () => {
+    menuBtn.classList.toggle("active");
+    closeBtn.classList.toggle("active");
+    navigation.classList.toggle("active");
+});
+
+navigationItems.forEach((navigationItem) => {
+    navigationItem.addEventListener("click", () => {
+        menuBtn.classList.remove("active");
+        closeBtn.classList.remove("active");
+        navigation.classList.remove("active");
+    });
+});
